9dd80b33eb6dc61ddeefbb371572fa8ce24324fb
Marc <marc@unit37.org>
2022-09-29 13:19:08 +0300

    adjust and minimize structure

---
+++ Python/argos.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+import argparse
+import netaddr
+import sys
+import logging
+from scapy.all import *
+from pprint import pprint
+from logging.handlers import RotatingFileHandler
+
+NAME = "argos"
+DESCRIPTION = "Logging and analzying probeframes from WiFi clients"
+
+
+def main():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+
+
+if __name__ == '__main__':
+	main()
