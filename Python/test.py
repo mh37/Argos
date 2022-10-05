6a81a1f8090a0e6707cad958ca72be78c26e2ee6
marc <marc@unit37.org>
2022-10-05 11:42:38 +0300

    remove bash files, we go full python

--- Python/test.py
+++ Python/test.py
@@ -2,6 +2,21 @@
 
 #This file is purely for temporary testing purposes, Will be removed later.
 
-import re, os, sys, signal, threading, time, subprocess, argparse, itertools
+import re, os, socket, sys, signal, threading, time, subprocess, argparse, itertools
 
 
+def main():
+
+    print("Available network interface controllers:")
+    print(socket.if_nameindex())
+
+    selectedNIC = input("Which NIC should be used? (Type the name):")
+
+    os.syste4m("airmon-ng check kill")
+    os.system("airmon-ng start " + selectedNIC)
+    os.system("probequest " + selectedNIC)
+
+
+
+if __name__ == '__main__':
+    main()
