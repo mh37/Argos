55fef32c0a976bb91ff9286c7ab7bb365762ac70
Marc <marc@unit37.org>
2022-10-31 13:52:05 +0200

    remove outdated code

--- Python/argos.py
+++ Python/argos.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 
-
-
 import re, os, socket, sys, signal, threading, time, subprocess, argparse, itertools
 from subprocess import Popen, PIPE
 
-#TODO: Processing / Analyzing, Formatting / Output / Map Visualisation (wigly API call based on hotspot names)
-def main():
 
+def main():
+    # TODO: Processing / Analyzing, Formatting / Output / Map Visualisation (wigly API call based on hotspot names)
     data = []
 
     print("Welcome to Argos")
@@ -18,20 +16,8 @@ def main():
     selectedNIC = input("Which NIC should be used? (Type the name):")
 
     #putting the NIC in monitor mode
-    subprocess.run(["sudo", "bettercap", "-iface", selectedNIC, "wifi.recon", "on"])
-    #subprocess.run(["wifi.recon", "on"])
-
-def print_capturedData():
-
-
-#Retrieve vendor based on MAC address
-def check_vendor(mac):
-    with open('macvendors.txt', 'r') as f:
-        for line in f:
-            l = line.split('\t')
-            if l[0].lower() in mac.replace(':', '') :
-                return l[1].strip()
-    return 'N/A'
+    subprocess.run(["sudo", "bettercap", "-iface", selectedNIC])
+    subprocess.run("wifi.recon on")
 
 if __name__ == '__main__':
     main()
