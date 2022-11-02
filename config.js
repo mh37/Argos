c1d04b47083c2a0060c07f15630f4adbc9bd0b38
Marc <marc@unit37.org>
2022-11-02 17:07:30 +0200

    change lat and long to current location

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
                 "serverIp": "127.0.0.1",
                 "serverPort": "8888",
-                "defaultLat": "40.7128",
-                "defaultLong": "74.0059",
+                "defaultLat": "60.201790",
+                "defaultLong": "24.933960",
                 "defaultZoom": 8,
                 "wigleAuthToken": "<WIGLE_API_TOKEN_IN_BASE64>",
                 "googleMapsAPIKey": "<GOOGLE_MAPS_API_KEY>",
