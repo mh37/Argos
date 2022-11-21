6bd4c752df04b1144641e7a80c3e4d891c2a61dd
Marc <marc@unit37.org>
2022-11-21 13:22:33 +0200

    add comment about channels

--- argos.py
+++ argos.py
@@ -14,6 +14,7 @@ from http.client import HTTPSConnection
 import urllib
 from tornado import websocket, web, httpserver, ioloop
 
+#in case you need to hop through channels (2.4 and 5 GHz Europe)
 channels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 68, 96, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 132, 134, 136, 138, 140, 142, 144, 149, 151, 153, 155, 157, 159, 161, 165, 167, 169, 171, 173]
 
 class WebSocketSever(websocket.WebSocketHandler):
